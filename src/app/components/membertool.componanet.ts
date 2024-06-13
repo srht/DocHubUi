5d11930d9a71ce987fe68e57baeeab7d05e15a10
srht <srhtkrgl@gmail.com>
2024-06-13 23:59:01 +0300

    menu display change

--- src/app/components/membertool.componanet.ts
+++ src/app/components/membertool.componanet.ts
@@ -2,18 +2,23 @@ import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-membertool',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule,MatIconModule, MatButtonModule, MatIconModule, MatButtonModule, MatIconModule, MembertoolComponent],
   template: `
-    <div *ngIf="isLoggedIn()">
-      <button (click)="logout()">Logout</button>
-    </div>
-    <div *ngIf="!isLoggedIn()">
-      <button routerLink="/login">Login</button>
-    </div>
+    <button *ngIf="isLoggedIn()" (click)="logout()" mat-icon-button>
+    <mat-icon>logout</mat-icon>
+  </button>
+    <button *ngIf="!isLoggedIn()" routerLink="/login" routerLinkActive="true" mat-icon-button>
+    <mat-icon>login</mat-icon>
+  </button>
+  <button *ngIf="!isLoggedIn()" routerLink="/register" routerLinkActive="true"  mat-icon-button>
+    <mat-icon>how_to_reg</mat-icon>
+  </button>
   `
 })
 export class MembertoolComponent {
